53fba5075dd9bc3b33e0fd1a6b5bbdde44198a77
dias-jefferson <jeff.dias97@gmail.com>
2023-12-06 11:19:03 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,14 @@
+/*
+const state = {
+    view:{
+        squares: document.querySelectorAll('.squares'),
+        enemy: document.querySelector('.enemy'),
+        timeLeft: document.querySelector('#time-left'),
+        score: document.querySelector('#score'),
+    },
+
+    value:{
+
+    },
+} 
+*/
